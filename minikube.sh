f83234f02ed008c3989cdd6d90e81ee29b37a7ac
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-06-30 07:07:56 +0500

    package.json added.

--- minikube.sh
+++ minikube.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+eval $(minikube docker-env)
+docker build -t iq-frontend .
+export BRANCH=develop
+kubectl apply -f ./deployment.yaml
+kubectl rollout restart -n iq-${BRANCH} deployment/iq-frontend
