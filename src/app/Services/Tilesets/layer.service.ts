2ddd02c50231cd1ada3501816fe78d0b892a43f3
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-07-13 17:56:26 +0500

    layer.service.ts Updated.

--- src/app/Services/Tilesets/layer.service.ts
+++ src/app/Services/Tilesets/layer.service.ts
@@ -1,1 +1,6 @@
 import { Injectable } from '@angular/core';
+import Tileset from '../../_Interfaces/Tileset';
+import { getPaintAndLineWidth } from '../../_utils/paintHandler';
+import {  layerConfig } from 'src/app/_Types/types';
+@Injectable({
+videdIn: 'root'
