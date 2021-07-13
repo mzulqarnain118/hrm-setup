e1693b5077d341556db12cd0f7cf8db849e415dc
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-07-13 05:49:03 +0500

    layer.service.ts Updated.

--- src/app/Services/Tilesets/layer.service.ts
+++ src/app/Services/Tilesets/layer.service.ts
@@ -4,3 +4,18 @@ import { getPaintAndLineWidth } from '../../_utils/paintHandler';
 import {  layerConfig } from 'src/app/_Types/types';
 @Injectable({
 videdIn: 'root'
+})
+export class LayerService {
+
+  constructor() { }
+
+  AddFillLayer(tileset: Tileset, map: mapboxgl.Map) {
+    const layerType = 'fill';
+    const FillPrefix = layerType + tileset.name;
+
+    const { paint } = getPaintAndLineWidth(tileset.name, layerType, tileset.TilesetType);
+    let layerConfig: layerConfig = {
+      // TODO: work this ID out
+      id: FillPrefix,
+      type: 'fill',
+ileset.name,
