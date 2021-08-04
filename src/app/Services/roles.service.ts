2025418a625f5a88ab3b627393f50e9b0de2bb58
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-08-04 07:34:15 +0500

    roles.service.ts Updated.

--- src/app/Services/roles.service.ts
+++ src/app/Services/roles.service.ts
@@ -1,1 +1,7 @@
 import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { DashboardService } from './Dashboard/dashboard.service';
+import { RoleTypes } from 'src/app/_Types/types';
+import { SharedStateService } from './shared-state.service';
+import { roles } from '../_Interfaces/dashboard';
+ctable({
