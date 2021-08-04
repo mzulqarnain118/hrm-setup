98511ebbc1b90c0e058caa13c647c5372603bd70
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-08-05 00:12:25 +0500

    roles.service.ts Updated.

--- src/app/Services/roles.service.ts
+++ src/app/Services/roles.service.ts
@@ -5,3 +5,26 @@ import { RoleTypes } from 'src/app/_Types/types';
 import { SharedStateService } from './shared-state.service';
 import { roles } from '../_Interfaces/dashboard';
 ctable({
+  providedIn: 'root',
+})
+export class RoleService {
+  private roles = new BehaviorSubject<roles[]>([]);
+  roles$ = this.roles.asObservable().pipe();
+  private dashboardService = inject(DashboardService);
+  private sharedService = inject(SharedStateService);
+
+  public fetchRoles(): Observable<roles[]> {
+    return this.dashboardService.getRoles();
+  }
+
+  isAccess(
+    box: string,
+    rolesData: roles[],
+    roleType: RoleTypes = 'product_name',
+  ): boolean {
+    if (box == 'dashboard') return true;
+    if (!rolesData) {
+      return false;
+    }
+    return rolesData.some((res) =>
+werCase()
