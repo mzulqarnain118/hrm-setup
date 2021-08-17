99a13cd3e9123681d58650652e6ebbddee36b86a
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-08-17 08:49:31 +0500

    shared-state.service.ts Updated.

--- src/app/Services/shared-state.service.ts
+++ src/app/Services/shared-state.service.ts
@@ -1,1 +1,36 @@
 import { Injectable } from '@angular/core';
+import { NavigationEnd, ResolveEnd, Router } from '@angular/router';
+import {
+  BehaviorSubject,
+  ReplaySubject,
+  Subject,
+  distinctUntilChanged,
+  filter,
+} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SharedStateService {
+  public isEsurveyVisible: boolean;
+  public combinedTilesetNames: string = '';
+
+  public TableEmissions: boolean = true;
+  private clickedBoundaryId = new BehaviorSubject<string>('1');
+  clickedBoundaryId$ = this.clickedBoundaryId
+    .asObservable()
+    .pipe(distinctUntilChanged());
+
+  private clickedBoundaryName = new BehaviorSubject<string>('');
+  clickedBoundaryName$ = this.clickedBoundaryName.asObservable().pipe();
+
+  private currentSeason = new BehaviorSubject<string>('');
+  currentSeason$ = this.currentSeason.asObservable().pipe();
+
+  private usernameSubject: BehaviorSubject<string> =
+    new BehaviorSubject<string>('');
+  usernameObservable = this.usernameSubject.asObservable();
+
+  public loaderSubject = new BehaviorSubject<boolean>(true);
+  loader$ = this.loaderSubject.asObservable();
+orSubject<boolean>(false);
