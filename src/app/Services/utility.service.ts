254c8229c94b17c8afb3fee2f061a216d35165c1
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-09-01 05:21:22 +0500

    utility.service.ts Updated.

--- src/app/Services/utility.service.ts
+++ src/app/Services/utility.service.ts
@@ -1,1 +1,15 @@
 import { Injectable } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import * as turf from '@turf/turf';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UtilityService {
+  constructor() {}
+
+  static calculateCenterForPolygon(feature: any): mapboxgl.LngLat {
+    const bounds = new mapboxgl.LngLatBounds();
+    (feature.geometry as any).coordinates[0].forEach((coord: any) => {
+      bounds.extend(coord);
+nst center = bounds.getCenter();
