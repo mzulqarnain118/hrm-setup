418c0c1e1a5a6764d94319120dc9d633d0e1a30f
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-09-03 20:38:33 +0500

    utility.service.ts Updated.

--- src/app/Services/utility.service.ts
+++ src/app/Services/utility.service.ts
@@ -13,3 +13,41 @@ export class UtilityService {
     (feature.geometry as any).coordinates[0].forEach((coord: any) => {
       bounds.extend(coord);
 nst center = bounds.getCenter();
+    return center;
+  }
+
+  static calculateCenterForFeatureCollection(featureCollection: any) {
+    const centroid = turf.centroid(featureCollection);
+    const [longitude, latitude] = centroid.geometry.coordinates;
+    return new mapboxgl.LngLat(longitude, latitude);
+  }
+
+  static preparePopup(popupHtml: string, popupDiv: HTMLElement | null) {
+        if (popupDiv) {
+      popupDiv.innerHTML = popupHtml;
+      popupDiv.style.opacity = '1';
+    }
+  }
+
+  static CalculateBounds(geojson: turf.FeatureCollection): mapboxgl.LngLatBounds {
+    const bounds = new mapboxgl.LngLatBounds();
+
+    geojson.features.forEach(feature => {
+      const geometry: turf.Geometry = feature.geometry as turf.Geometry;
+      if (geometry.type === 'MultiPolygon') {
+        const multiPolygon: turf.MultiPolygon = geometry as turf.MultiPolygon;
+        multiPolygon.coordinates.forEach(polygon => {
+          polygon.forEach(ring => {
+            ring.forEach(coord => {
+              bounds.extend(coord as [number, number]);
+            });
+          });
+        });
+      }
+    });
+
+    return bounds;
+  }
+
+  static AddMarkerToMap(){
+  }
