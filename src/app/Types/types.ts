47b0d59da12537a460dc9419965dde265b5afcac
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-09-03 17:36:42 +0500

    types.ts added.

---
+++ src/app/Types/types.ts
@@ -0,0 +1,22 @@
+import * as mapboxgl from 'mapbox-gl';
+
+export type SourceType = 'Tileset' | 'Boundary' | 'Esurvey' | 'Raster';
+export type LayerType = 'fill' | 'line' | 'fill-extrusion';
+export type layerConfig = {
+  id: string;
+  type: LayerType;
+  source: string;
+  paint: mapboxgl.AnyPaint;
+  layout: mapboxgl.Layout;
+  'source-layer'?: string;
+  filter?: mapboxgl.Expression;
+};
+
+/**
+ * Esurvey is the custom type for the generics 
+ */
+export type TilesetType = "Raster" | "Vector" | "Esurvey"
+
+
+export type RoleTypes = "product_name" | "platfrom_name"
+
