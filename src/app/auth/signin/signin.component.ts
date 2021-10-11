aebea1da858241f2a4979d75ef2deeafa995b6c7
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-10-11 10:36:43 +0500

    , signin.component.ts, console.component.html, console.component.scss, console.component.spec.ts, console.component.ts, console.module.ts, cropscan.module.ts, dropdown.component.html, dropdown.component.scss, dropdown.component.spec.ts - Files added.

---
+++ src/app/auth/signin/signin.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormComponent } from '../form/form.component';
+
+@Component({
+    selector: 'app-signin',
+    templateUrl: './signin.component.html',
+    styleUrls: ['./signin.component.scss'],
+    standalone: true,
+    imports: [FormComponent],
+})
+export class SigninComponent {
+  constructor() {}
+}
