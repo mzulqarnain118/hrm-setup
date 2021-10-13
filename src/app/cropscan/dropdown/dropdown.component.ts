41c7fe3da2fc3ceb883bb07c230fee1a03538520
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-10-13 13:45:18 +0500

    dropdown.component.ts Updated.

--- src/app/cropscan/dropdown/dropdown.component.ts
+++ src/app/cropscan/dropdown/dropdown.component.ts
@@ -118,3 +118,39 @@ import {
 } from 'rxjs';
 import { ApiService } from 'src/app/_Services/api.service';
 Service } from 'src/app/_Services/map.service';
+import { SharedStateService } from 'src/app/_Services/shared-state.service';
+import { SidepanelService } from 'src/app/_Services/sidepanel.service';
+import { AsyncPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+export interface DropdownData {}
+
+@Component({
+    selector: 'app-dropdown',
+    templateUrl: './dropdown.component.html',
+    styleUrls: ['./dropdown.component.scss'],
+    standalone: true,
+    imports: [FormsModule, AsyncPipe],
+})
+export class DropdownComponent {
+  // Selected value
+  currentSeason$ = new Observable<string>();
+  currentDate$ = new Observable<string>();
+  currentCrop$ = new Observable<string>();
+
+  // Options from backend
+  seasonsOptions$ = new Observable<string[]>();
+  cropOptions$ = new Observable<string[]>();
+  dateOptions$ = new Observable<string[]>();
+
+  combinedObservables$ = new Observable<string[]>();
+
+  constructor(
+    private apiService: ApiService,
+    private mapService: MapService,
+    private sidePanelService: SidepanelService,
+    private sharedStateService: SharedStateService
+  ) {}
+
+  ngOnInit(): void {
+combinedObservables$ = combineLatest([
