f66bc7c11da297416ed4fea5b2217b3865f94d58
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-10-15 13:42:57 +0500

    dropdown.component.ts Updated.

--- src/app/cropscan/dropdown/dropdown.component.ts
+++ src/app/cropscan/dropdown/dropdown.component.ts
@@ -1,1 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  Observable,
+  combineLatest,
+  distinctUntilChanged,
+  map,
+  of,
+  shareReplay,
+  takeLast,
+  tap,
+} from 'rxjs';
+import { ApiService } from 'src/app/_Services/api.service';
+import { MapService } from 'src/app/_Services/map.service';
+import { SharedStateService } from 'src/app/_Services/shared-state.service';
+import { SidepanelService } from 'src/app/_Services/sidepanel.service';
+import { AsyncPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+export interface DropdownData {}
+
+@Component({
+    selector: 'app-dropdown',
+    templateUrl: './dropdown.component.html',
+    styleUrls: ['./dropdown.component.scss'],
+    standalone: true,
+    imports: [FormsModule, AsyncPipe],
+mponent {
