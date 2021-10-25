80b819a7c9dfbec0e0e0b56789abd73b10cbe8a8
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-10-25 08:29:00 +0500

    export-data.component.spec.ts Updated.

--- src/app/cropscan/export-data/export-data.component.spec.ts
+++ src/app/cropscan/export-data/export-data.component.spec.ts
@@ -1,1 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExportDataComponent } from './export-data.component';
+
+describe('ExportDataComponent', () => {
+  let component: ExportDataComponent;
+  let fixture: ComponentFixture<ExportDataComponent>;
+
+Each(() => {
+    TestBed.configureTestingModule({
+    imports: [ExportDataComponent]
+});
+    fixture = TestBed.createComponent(ExportDataComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
