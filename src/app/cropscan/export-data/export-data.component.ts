fa506b69069f2d57e061bdc6e4e0d79e668afbd8
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-10-27 01:43:24 +0500

    export-data.component.ts Updated.

--- src/app/cropscan/export-data/export-data.component.ts
+++ src/app/cropscan/export-data/export-data.component.ts
@@ -1,1 +1,15 @@
 import { Component, Renderer2 } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SharedStateService } from 'src/app/_Services/shared-state.service';
+import { environment } from 'src/environments/environment';
+@Component({
+    selector: 'app-export-data',
+    templateUrl: './export-data.component.html',
+    styleUrls: ['./export-data.component.scss'],
+    standalone: true,
+})
+export class ExportDataComponent {
+  constructor(
+    private http: HttpClient,
+    private renderer: Renderer2,
+aredStateService: SharedStateService
