1a169a56f46f0d7132d761878e92d00550a980e2
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-12-07 06:36:57 +0500

    search.component.spec.ts Updated.

--- src/app/cropscan/search/search.component.spec.ts
+++ src/app/cropscan/search/search.component.spec.ts
@@ -20,3 +20,22 @@ describe('SearchComponent', () => {
   });
 });
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchComponent } from './search.component';
+scribe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [SearchComponent]
+});
+    fixture = TestBed.createComponent(SearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
