235607d3c2a1e723d6ae5a59dc4d1b503b12af19
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2021-12-23 06:05:11 +0500

    t1-areas.component.ts Updated.

--- src/app/cropscan/t1-areas/t1-areas.component.ts
+++ src/app/cropscan/t1-areas/t1-areas.component.ts
@@ -49,3 +49,53 @@ export class T1AreasComponent {
   prevScrollPosition: number = 0;
   maxCount: number = 0;
 me$ = new Observable<string>();
+  reportType: string;
+  season: string;
+  reportDate: string;
+  currentBoundary: string;
+  boundrayName: string;
+  key: string;
+  crop: string;
+  getValue(object: any, key: string): any {
+    return object?.[key];
+  }
+
+  constructor(
+    private SidepanelService: SidepanelService,
+    private apiService: ApiService,
+    private roleService: RoleService,
+    private cdr: ChangeDetectorRef,
+  ) {
+    this.clickBoundaryName$ = this.SidepanelService.clickedBoundaryName$;
+    this.combinedData$ = this.SidepanelService.combinedData$;
+    this.combinedData$
+      .pipe(
+        filter((res) => res.every((element) => element !== '')),
+        debounceTime(300),
+        switchMap((res) => {
+          const [crop, reportType, season, reportDate, currentBoundary] = res;
+          this.crop = crop;
+          this.reportType = reportType;
+          this.season = season;
+          this.reportDate = reportDate;
+          this.currentBoundary = currentBoundary;
+          this.offset = 0;
+
+          this.lastBoundaryName = currentBoundary;
+
+          this.key = JSON.stringify({
+            crop: this.crop,
+            reportType: this.reportType,
+            season: this.season,
+            reportDate: this.reportDate,
+            currentBoundary: this.currentBoundary,
+            limit: this.limit,
+            offset: this.offset,
+          });
+
+          const memoizedObservable = this.memoizationService.memoize(
+            this.key,
+            this.apiService.getT1Data(
+              crop,
+              this.reportType,
+rtDate,
