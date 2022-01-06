4d333f58903ff5439f30d13db7eaba649eeef27d
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-01-07 00:29:26 +0500

    t3-bar-chart.component.spec.ts Updated.

--- src/app/cropscan/t3-bar-chart/t3-bar-chart.component.spec.ts
+++ src/app/cropscan/t3-bar-chart/t3-bar-chart.component.spec.ts
@@ -1,1 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { T3BarChartComponent } from './t3-bar-chart.component';
+
+describe('T3BarChartComponent', () => {
+  let component: T3BarChartComponent;
+  let fixture: ComponentFixture<T3BarChartComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [T3BarChartComponent]
+});
+    fixture = TestBed.createComponent(T3BarChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
