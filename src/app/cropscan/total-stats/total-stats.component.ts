1f8444cde18e2627b79258f600c54250b9e5e749
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-02-22 21:08:58 +0500

    total-stats.component.ts Updated.

--- src/app/cropscan/total-stats/total-stats.component.ts
+++ src/app/cropscan/total-stats/total-stats.component.ts
@@ -1,1 +1,9 @@
 import { Component } from '@angular/core';
+import { Subject, combineLatest, filter, map, switchMap } from 'rxjs';
+import { ApiService } from 'src/app/_Services/api.service';
+import { SharedStateService } from 'src/app/_Services/shared-state.service';
+import { AddCommasPipe } from '../../_Pipes/add-commas.pipe';
+import { NgClass, AsyncPipe, KeyValuePipe } from '@angular/common';
+
+export interface TotalStatsData {}
+nt({
