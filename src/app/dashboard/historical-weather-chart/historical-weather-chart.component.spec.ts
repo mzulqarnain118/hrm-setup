51930454ea085762330977e72aab44ec4e580b0a
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-03-25 06:11:06 +0500

    historical-weather-chart.component.ts added.

--- src/app/dashboard/historical-weather-chart/historical-weather-chart.component.spec.ts
+++ src/app/dashboard/historical-weather-chart/historical-weather-chart.component.spec.ts
@@ -1,1 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HistoricalWeatherChartComponent } from './historical-weather-chart.component';
+
+describe('HistoricalWeatherChartComponent', () => {
+  let component: HistoricalWeatherChartComponent;
+  let fixture: ComponentFixture<HistoricalWeatherChartComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [HistoricalWeatherChartComponent]
+});
+    fixture = TestBed.createComponent(HistoricalWeatherChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
