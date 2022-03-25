950c6c648ce7ad667c37b3d0b28f905e43b4f348
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-03-25 10:15:51 +0500

    historical-weather-chart.component.ts Updated.

--- src/app/dashboard/historical-weather-chart/historical-weather-chart.component.ts
+++ src/app/dashboard/historical-weather-chart/historical-weather-chart.component.ts
@@ -1,1 +1,39 @@
 import { Component } from '@angular/core';
+import { Color, ScaleType, LineChartModule } from '@swimlane/ngx-charts';
+
+@Component({
+    selector: 'app-historical-weather-chart',
+    templateUrl: './historical-weather-chart.component.html',
+    styleUrls: ['./historical-weather-chart.component.scss'],
+    standalone: true,
+    imports: [LineChartModule],
+})
+export class HistoricalWeatherChartComponent {
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  showYAxisLabel: boolean = true;
+  yAxisLabel: string = 'Average Temperature';
+  colorScheme: Color = {
+    name: 'Variation',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: ['#1BC693'],
+  };
+  activeItem: string = '1W';
+  isActive(item: string): boolean {
+    return this.activeItem === item;
+  }
+
+  setIsActive(item: string) {
+    this.activeItem = item;
+  }
+  weatherLineChart = [
+    {
+      name: 'Weather',
+      series: [
+        {
+          name: 'Jan',
+  {
