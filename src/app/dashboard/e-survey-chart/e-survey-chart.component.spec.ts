aefd22d3707a15b0b52575426818ac23c00a28af
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-04-04 17:07:52 +0500

    e-survey-chart.component.spec.ts Updated.

--- src/app/dashboard/e-survey-chart/e-survey-chart.component.spec.ts
+++ src/app/dashboard/e-survey-chart/e-survey-chart.component.spec.ts
@@ -1,1 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ESurveyChartComponent } from './e-survey-chart.component';
+
+describe('ESurveyChartComponent', () => {
+  let component: ESurveyChartComponent;
+  let fixture: ComponentFixture<ESurveyChartComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+s: [ESurveyChartComponent]
+});
+    fixture = TestBed.createComponent(ESurveyChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
