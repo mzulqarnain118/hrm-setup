65ef2f2eda0c9077535d6181bf1bab9f898d8640
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-04-26 14:01:37 +0500

    weather-forecast-chart.component.ts Updated.

--- src/app/dashboard/weather-forecast-chart/weather-forecast-chart.component.ts
+++ src/app/dashboard/weather-forecast-chart/weather-forecast-chart.component.ts
@@ -1,1 +1,16 @@
 import { Component } from '@angular/core';
+import { Color, ScaleType, LineChartModule } from '@swimlane/ngx-charts';
+
+@Component({
+    selector: 'app-weather-forecast-chart',
+    templateUrl: './weather-forecast-chart.component.html',
+    styleUrls: ['./weather-forecast-chart.component.scss'],
+    standalone: true,
+    imports: [LineChartModule],
+})
+export class WeatherForecastChartComponent {
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = true;
+  showLegend: boolean = true;
+l: boolean = true;
