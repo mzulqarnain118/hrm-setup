7cc54e42762278555a107cc16d1bd6710b49398e
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-05-13 23:14:21 +0500

    ndvi-chart.component.spec.ts added.

---
+++ src/app/yieldpro/ndvi-chart/ndvi-chart.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NdviChartComponent } from './ndvi-chart.component';
+
+describe('NdviChartComponent', () => {
+  let component: NdviChartComponent;
+  let fixture: ComponentFixture<NdviChartComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [NdviChartComponent]
+});
+    fixture = TestBed.createComponent(NdviChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
