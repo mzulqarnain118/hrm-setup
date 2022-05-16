6026c11b397c5f62a5da1122c60c80423bd82415
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-05-17 03:47:25 +0500

    ndvi-chart.component.ts Updated.

--- src/app/yieldpro/ndvi-chart/ndvi-chart.component.ts
+++ src/app/yieldpro/ndvi-chart/ndvi-chart.component.ts
@@ -1,1 +1,22 @@
 import { Component } from '@angular/core';
+import { Color, ScaleType, LineChartModule } from '@swimlane/ngx-charts';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+
+@Component({
+    selector: 'app-ndvi-chart',
+    templateUrl: './ndvi-chart.component.html',
+    styleUrls: ['./ndvi-chart.component.scss'],
+    standalone: true,
+    imports: [NgClass, FormsModule, LineChartModule]
+})
+export class NdviChartComponent {
+  single: any[];
+  protected view: any[] = [700, 400];
+  protected showXAxis = true;
+  protected showYAxis = true;
+  protected gradient = false;
+  protected showLegend = false;
+  protected showXAxisLabel = true;
+  protected xAxisLabel: "Years";
+Label = true;
