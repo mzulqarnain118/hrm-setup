b861db3e9732f70385786e30bc3f2755813c73d8
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-05-17 18:14:13 +0500

    global-navbar.component.ts Updated.

--- src/app/global-navbar/global-navbar.component.ts
+++ src/app/global-navbar/global-navbar.component.ts
@@ -11,3 +11,68 @@ import { LogoComponent } from '../cropscan/logo/logo.component';
 import { NgClass, AsyncPipe } from '@angular/common';
 
 
+    selector: 'app-global-navbar',
+    templateUrl: './global-navbar.component.html',
+    styleUrls: ['./global-navbar.component.scss'],
+    standalone: true,
+    imports: [
+        NgClass,
+        LogoComponent,
+        AsyncPipe,
+    ],
+})
+export class GlobalNavbarComponent implements OnInit {
+  roles: roles[];
+  private user = inject(UserService);
+  protected sharedService = inject(SharedStateService);
+  protected roleService = inject(RoleService);
+  private router = inject(Router);
+  username$ = this.sharedService.usernameObservable;
+
+  menus = menuList;
+  activeItem!: string;
+
+  constructor() {
+    const username = localStorage.getItem('iq-username');
+    if (username) {
+      this.sharedService.updateUsername(username);
+    }
+    let fetchCalled = false;
+    combineLatest([this.sharedService.menu$, this.roleService.roles$]).pipe(
+      filter(([menu, roles]) => menu !== null && roles !== null),
+      map(([menu, roles]) => {
+        this.activeItem = menu;
+        this.roles = roles;
+        
+        if (roles.length === 0 && !fetchCalled && !this.isLoginPage()) {
+          fetchCalled=true;
+          this.roleService.fetchRoles().subscribe((response) => {
+            this.roleService.updateRoles(response);
+            this.roles = response;
+subscribe();
+  }
+
+  ngOnInit(): void {
+    this.sharedService.menu$.subscribe((res) => {
+      this.activeItem = res;
+    });
+  }
+
+  isLoginPage(): boolean {
+    let isAuthenticated = localStorage.getItem('isAuthenticated');
+    return (
+      !this.menus.some((menu) => this.router.url.includes(menu.key)) ||
+      !isAuthenticated
+    );
+  }
+
+  setIsActive(item: string, isAccess: boolean) {
+    if (!isAccess) return;
+    this.sharedService.updateMenu(item);
+    this.router.navigateByUrl(item);
+  }
+
+  signOut() {
+    this.user.logout();
+  }
+}
