3ce3510c7e31c33d1fce6258d15f552ab8056d7b
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-05-24 11:45:22 +0500

    toPascalNotation.ts added.

---
+++ src/app/utils/toPascalNotation.ts
@@ -0,0 +1,13 @@
+export function toPascalCase(value: string) {
+  return value === 'aoi'
+    ? value.toLocaleUpperCase()
+    : `${value}`
+        .toLowerCase()
+        .replace(new RegExp(/[-_]+/, 'g'), ' ')
+        .replace(new RegExp(/[^\w\s]/, 'g'), '')
+        .replace(
+          new RegExp(/\s+(.)(\w*)/, 'g'),
+          ($1, $2, $3) => `${$2.toUpperCase() + $3}`
+        )
+        .replace(new RegExp(/\w/), (s) => s.toUpperCase());
+}
