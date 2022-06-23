307384297cda20b524b48c1d4acb49432b43a65b
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-06-23 23:13:50 +0500

    sidepanel.component.ts Updated.

--- src/app/yieldpro/sidepanel/sidepanel.component.ts
+++ src/app/yieldpro/sidepanel/sidepanel.component.ts
@@ -1,1 +1,5 @@
 import { Component } from '@angular/core';
+import { SharedStateService } from 'src/app/_Services/shared-state.service';
+import { NdviTableComponent } from '../ndvi-table/ndvi-table.component';
+import { T2CropsComponent } from '../../cropscan/t2-crops/t2-crops.component';
+rt { SelectionBarComponent } from '../../cropscan/selection-bar/selection-bar.component';
