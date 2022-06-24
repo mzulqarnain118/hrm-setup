10855fb7dff1ed79cd7bf330b20b366a640f994e
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-06-24 05:50:13 +0500

    yieldpro.component.html added.

--- src/app/yieldpro/sidepanel/sidepanel.component.ts
+++ src/app/yieldpro/sidepanel/sidepanel.component.ts
@@ -3,3 +3,24 @@ import { SharedStateService } from 'src/app/_Services/shared-state.service';
 import { NdviTableComponent } from '../ndvi-table/ndvi-table.component';
 import { T2CropsComponent } from '../../cropscan/t2-crops/t2-crops.component';
 rt { SelectionBarComponent } from '../../cropscan/selection-bar/selection-bar.component';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+    selector: 'app-sidepanels',
+    templateUrl: './sidepanel.component.html',
+    styleUrls: ['./sidepanel.component.scss'],
+    standalone: true,
+    imports: [FormsModule, SelectionBarComponent, T2CropsComponent, NdviTableComponent]
+})
+export class SidepanelComponent {
+  constructor(protected sharedState: SharedStateService) {
+  }
+  collapseSideBar() {
+    this.sharedState.updatedIsCollapse(true);
+  }
+
+    handleSelection(event: Event): void {
+      const target = event.target as HTMLSelectElement;
+      const reportType = target.value;
+    }
+}
