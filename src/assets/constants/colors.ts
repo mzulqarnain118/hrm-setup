1b33ef450ac405ad5195cbe94a26e4efdbd15f2b
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-07-06 10:02:17 +0500

    colors.ts Updated.

--- src/assets/constants/colors.ts
+++ src/assets/constants/colors.ts
@@ -1,1 +1,49 @@
 // TODO: NEED TO UNIFY THE COLOR CODES
+
+// Color constants
+export const EsurveyBoundaryColor = '#B200ED';
+export const EsurveyBoundaryWidth = 2;
+
+// Transparent color
+export const TransparentColor = '#00000000';
+
+// Normal Boundary color
+export const BoundaryColor = '#0018F9';
+export const BoundaryColorHover = '#FF2400';
+
+// Other variety color
+export const OtherColor = '#A0A0A0';
+export const OtherVegetationColor = '#e9ed2c';
+// Crops color
+export const SugarCaneColor = '#83F28F';
+export const MaizeColor = '#00FFFF';
+export const CottonColor = '#FFFFFF';
+export const TomatoColor = '#C62828';
+export const WheatColor = '#A3F307';
+export const OrchardsColor = '#6A1B9A';
+export const BananaColor = '#FF6A02';
+export const CanolaColor = '#29B6F6';
+export const RiceColor = '#1B5E20';
+export const ChilliColor = '#F50B86';
+export const UrbanColor = '#151515';
+export const WatermelonColor = '#FC6C85';
+export const TemporalColor = '#FF0000';
+// brownish color
+export const PotatoColor = '#795C34';
+
+// grenhouse color
+export const GreenhouseColor = '#7CFC00';
+// Harvested color
+export const HarvestedColor = '#F89508';
+
+export const ArtichokeColor = '#3c5426';
+export const GrapesColor = '#e30022';
+export const OliveTreeColor = '#54262b';
+export const JojobaColor = '#ff007f';
+export const AsparagusColor = '#100c08';
+export const TangerineColor = '#ff4f00';
+export const AvocadoColor = '#1f2dea';
+// normal colors
+export const CloudColor = '#D5D8DC';
+export const white = '#FFFFFF';
+A', // Yield <500
