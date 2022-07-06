0da2e30597b8562a599f63459a249f6b3e50abff
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-07-06 17:20:43 +0500

    constants.ts Updated.

--- src/assets/constants/constants.ts
+++ src/assets/constants/constants.ts
@@ -1,1 +1,12 @@
 import Tileset from 'src/app/_Interfaces/Tileset';
+
+export const DEFAULT_REPORT = 'Crop Scan';
+export const PAKISTAN_CENTER = [69.3941914, 29.9689954];
+export const MAPBOX_USERNAME = 'ali-akber-79';
+
+// TODO : Change to this a backend API call
+export const BASE_STYLE_URL =
+  'mapbox://styles/ali-akber-79/clmqh6svs026o01r46xxxg1hg';
+export const DEFAULT_ZOOM = 3.1;
+export const AOI_ZOOM = 5;
+t Esurvey_ZOOM = 10;
