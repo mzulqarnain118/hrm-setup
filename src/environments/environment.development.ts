3c11e9bbb12079e44654afb26a4b0cf2930b2b1d
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-08-17 09:11:18 +0500

    environment.staging.ts added.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,8 @@
 export const environment = {
+    production: false,
+    AuthServiceURL: 'http://localhost:5100/iq/api',
+    BACKEND_URL: 'http://localhost:5001/cropscan/api',
+    AccessToken:
+      'pk.eyJ1IjoiYWxpLWFrYmVyLTc5IiwiYSI6ImNsYXRqMTB0bzAwY3Izdm55Zmptc2N6ZjkifQ.T28yDHgDc28SCV96kH5NUg',
+  };
+  
