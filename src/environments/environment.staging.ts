54a10ce93238053bb9958f2b150c55307979711c
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2022-08-18 13:38:32 +0500

    environment.ts added.

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,1 +1,7 @@
 export const environment = {
+  production: false,
+  AuthServiceURL: 'http://localhost:5100',
+  BACKEND_URL: 'http://54.253.64.209',
+  AccessToken:
+    'pk.eyJ1IjoiYWxpLWFrYmVyLTc5IiwiYSI6ImNsYXRqMTB0bzAwY3Izdm55Zmptc2N6ZjkifQ.T28yDHgDc28SCV96kH5NUg',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  AuthServiceURL: 'https://app.farmdar.ai/iq/api',
+  BACKEND_URL: 'https://app.farmdar.ai/cropscan/api',
+  AccessToken:
+    'pk.eyJ1IjoiYWxpLWFrYmVyLTc5IiwiYSI6ImNsYXRqMTB0bzAwY3Izdm55Zmptc2N6ZjkifQ.T28yDHgDc28SCV96kH5NUg'
+};
