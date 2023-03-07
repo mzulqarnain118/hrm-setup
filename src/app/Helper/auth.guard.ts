373c31c1f849109b7c61eff349d66383b6833094
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-03-08 04:26:29 +0500

    auth.guard.ts added.

--- src/app/Helper/auth.guard.ts
+++ src/app/Helper/auth.guard.ts
@@ -13,3 +13,18 @@ export const authGuard: CanActivateFn = () => {
   router.navigate(['/signin']);
   return false;
 };
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '../auth/services/user.service';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = () => {
+  const userService = inject(UserService);
+  const router = inject(Router);
+
+  if (userService.isAuthenticated()) {
+    return true;
+  }
+
+  router.navigate(['/signin']);
+  return false;
+};
