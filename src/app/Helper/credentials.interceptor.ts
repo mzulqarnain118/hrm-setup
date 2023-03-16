a2db242640e0cb8a1f2824aae1428add89bedc85
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-03-17 02:47:38 +0500

    credentials.interceptor.ts Updated.

--- src/app/Helper/credentials.interceptor.ts
+++ src/app/Helper/credentials.interceptor.ts
@@ -25,3 +25,28 @@ intercepted request', request.url);
 }
 
 import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CredentialInterceptor implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const url = new URL(request.url, 'http://localhost:4200');
+    if (!url.pathname.startsWith('/cropscan/api')) return next.handle(request);
+    console.log('intercepted request', request.url);
+ithCredentials = request.clone({
+      withCredentials: true,
+    });
+
+    return next.handle(requestWithCredentials);
+  }
+}
+
