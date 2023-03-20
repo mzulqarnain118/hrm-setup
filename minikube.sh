dd8891be5e2d7ab35e0daa40cb0b4ea581d9a308
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-03-20 10:50:56 +0500

    minikube.sh added.

--- minikube.sh
+++ minikube.sh
@@ -4,3 +4,9 @@ docker build -t iq-frontend .
 export BRANCH=develop
 kubectl apply -f ./deployment.yaml
 kubectl rollout restart -n iq-${BRANCH} deployment/iq-frontend
+#!/bin/bash
+eval $(minikube docker-env)
+docker build -t iq-frontend .
+export BRANCH=develop
+kubectl apply -f ./deployment.yaml
+kubectl rollout restart -n iq-${BRANCH} deployment/iq-frontend
