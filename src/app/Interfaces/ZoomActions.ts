6494579b0fee343e2807c91e6fab98d55eb0988b
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-03-31 01:22:26 +0500

    ZoomActions.ts added.

--- src/app/Interfaces/ZoomActions.ts
+++ src/app/Interfaces/ZoomActions.ts
@@ -5,3 +5,9 @@ export default interface ZoomActions {
     add: () => void;
     currentLevel: string;
   }
+
+export default interface ZoomActions {
+    range: number[];
+    // this is a function that adds and removes layers
+    add: () => void;
+    currentLevel: string;
