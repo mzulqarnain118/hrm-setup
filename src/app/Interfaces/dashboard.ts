2ec1498dbcd24c77facc16031e958317d43d2bdc
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-04-05 13:27:43 +0500

    dashboard.ts added.

--- src/app/Interfaces/dashboard.ts
+++ src/app/Interfaces/dashboard.ts
@@ -43,3 +43,25 @@ export interface BoxItem {
   url?: string;
   product_name: string;
 }
+export interface roles {
+  access: boolean;
+  id: number;
+  operation: string;
+  product_id: number;
+  product_name: string;
+  platform_name:string;
+}
+
+
+export interface MenuItem {
+  key: string;
+  icon: string;
+  title: string;
+}
+
+export interface BoxItem {
+  title: string;
+  count: number | string;
+  type: string;
+  url?: string;
+  product_name: string;
