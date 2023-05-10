43b8493f42df6bcd7b66948f0246c996e8afe8fa
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-05-10 05:21:48 +0500

    app.component.ts added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,3 +22,15 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
   }
 }
+import { Component, inject, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'IQ-Frontend';
+
+  ngOnInit(): void {
+  }
+}
