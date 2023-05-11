18f84abdcca9386c138dcffbd35deace1c0b64f0
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-05-11 23:09:05 +0500

    types.ts Updated.

--- src/app/Types/types.ts
+++ src/app/Types/types.ts
@@ -22,3 +22,24 @@ export type RoleTypes = "product_name" | "platfrom_name"
 
 export type SearchOption = "Coordinate" | "SurveyId"
 import * as mapboxgl from 'mapbox-gl';
+
+export type SourceType = 'Tileset' | 'Boundary' | 'Esurvey' | 'Raster';
+export type LayerType = 'fill' | 'line' | 'fill-extrusion';
+rt type layerConfig = {
+  id: string;
+  type: LayerType;
+  source: string;
+  paint: mapboxgl.AnyPaint;
+  layout: mapboxgl.Layout;
+  'source-layer'?: string;
+  filter?: mapboxgl.Expression;
+};
+
+/**
+ * Esurvey is the custom type for the generics 
+ */
+export type TilesetType = "Raster" | "Vector" | "Esurvey"
+
+
+export type RoleTypes = "product_name" | "platfrom_name"
+
