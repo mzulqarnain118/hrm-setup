9c90df8ecc7aa486f94a5e9095a7aa2c689df1e8
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-05-15 02:43:34 +0500

    app-routing.module.ts Updated.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -53,3 +53,30 @@ path: 'console',
 })
 export class AppRoutingModule {}
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './_Helper/auth.guard';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { CropscanModule } from './cropscan/cropscan.module';
+import { YieldproModule } from './yieldpro/yieldpro.module';
+import { ROLE_GUARD_TOKEN } from './_Services/Dashboard/role.guard';
+import { ConsoleModule } from './console/console.module';
+import { SigninComponent } from './auth/signin/signin.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: 'signin', component:SigninComponent},
+
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'cropscan',
+    loadChildren: () =>
+      import('./cropscan/cropscan.module').then((m) => m.CropscanModule),
+    canActivate: [ROLE_GUARD_TOKEN, authGuard],
+    data: { roles: ['cropscan'] },
+  },
+   loadChildren: () =>
