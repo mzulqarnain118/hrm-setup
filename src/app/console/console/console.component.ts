0c1a3861c3bebfda00328fec5c904589e73e7ed4
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-05-31 01:02:31 +0500

    console.component.ts added.

---
+++ src/app/console/console/console.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-console',
+    templateUrl: './console.component.html',
+    styleUrls: ['./console.component.scss'],
+    standalone: true
+})
+export class ConsoleComponent {
+
+}
