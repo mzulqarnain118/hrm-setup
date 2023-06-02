1940381ad6242d72bdfed8a640803a49461233da
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-06-02 07:23:23 +0500

    cropscan.module.ts Updated.

--- src/app/cropscan/cropscan.module.ts
+++ src/app/cropscan/cropscan.module.ts
@@ -1,1 +1,20 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapLayoutComponent } from './map-layout/map-layout.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ROLE_GUARD_TOKEN } from '../_Services/Dashboard/role.guard';
+import { authGuard } from '../_Helper/auth.guard';
+
+
+const routes: Routes = [
+  {
+    path: 'cropscan',
+    component: MapLayoutComponent, canActivate:[ROLE_GUARD_TOKEN, authGuard],
+  },
+({
+  imports: [
+    [RouterModule.forChild(routes)],
+    CommonModule,
+  ]
+})
+export class CropscanModule { }
