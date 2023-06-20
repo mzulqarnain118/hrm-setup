6be14e008fd5853efcc2dd444da607d7baa48ce5
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-06-20 05:56:04 +0500

    , export-data.component.spec.ts, export-data.component.ts, layer-toggle.component.html, layer-toggle.component.scss, layer-toggle.component.spec.ts, layer-toggle.component.ts, logo.component.html, logo.component.scss, logo.component.ts - Files added.

--- src/app/cropscan/export-data/export-data.component.spec.ts
+++ src/app/cropscan/export-data/export-data.component.spec.ts
@@ -6,27 +6,6 @@ describe('ExportDataComponent', () => {
   let component: ExportDataComponent;
   let fixture: ComponentFixture<ExportDataComponent>;
 
-Each(() => {
-    TestBed.configureTestingModule({
-    imports: [ExportDataComponent]
-});
-    fixture = TestBed.createComponent(ExportDataComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ExportDataComponent } from './export-data.component';
-
-describe('ExportDataComponent', () => {
-  let component: ExportDataComponent;
-  let fixture: ComponentFixture<ExportDataComponent>;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
     imports: [ExportDataComponent]
