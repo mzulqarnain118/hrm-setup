59f2438ab3657efeb6db7131794d0ff94d4fd8e9
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-07-13 06:17:47 +0500

    layer-toggle.component.spec.ts added.

--- src/app/cropscan/layer-toggle/layer-toggle.component.spec.ts
+++ src/app/cropscan/layer-toggle/layer-toggle.component.spec.ts
@@ -19,3 +19,24 @@ ed.configureTestingModule({
     expect(component).toBeTruthy();
   });
 });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LayerToggleComponent } from './layer-toggle.component';
+
+describe('LayerToggleComponent', () => {
+  let component: LayerToggleComponent;
+  let fixture: ComponentFixture<LayerToggleComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [LayerToggleComponent]
+});
+    fixture = TestBed.createComponent(LayerToggleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
