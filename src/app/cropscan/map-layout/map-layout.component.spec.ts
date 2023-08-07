90bb29b87a58e6d64a521def959aed710ae55c63
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-08-07 13:57:58 +0500

    map-layout.component.spec.ts Updated.

--- src/app/cropscan/map-layout/map-layout.component.spec.ts
+++ src/app/cropscan/map-layout/map-layout.component.spec.ts
@@ -20,3 +20,22 @@ describe('MapLayoutComponent', () => {
   });
 });
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MapLayoutComponent } from './map-layout.component';
+
+describe('MapLayoutComponent', () => {
+  let component: MapLayoutComponent;
+  let fixture: ComponentFixture<MapLayoutComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [MapLayoutComponent]
+});
+    fixture = TestBed.createComponent(MapLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
