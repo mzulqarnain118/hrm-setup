761d924d4fd3f7d8eb7bc3fac7d9e78652ecfb85
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-08-07 14:53:14 +0500

    map-layout.component.ts added.

--- src/app/cropscan/map-layout/map-layout.component.ts
+++ src/app/cropscan/map-layout/map-layout.component.ts
@@ -39,3 +39,44 @@ export class MapLayoutComponent {
     this.sharedState.updatedIsCollapse(false);
   }
 }
+import { Component } from '@angular/core';
+import { MapService } from 'src/app/_Services/map.service';
+import { SharedStateService } from 'src/app/_Services/shared-state.service';
+import { MapComponent } from '../map/map.component';
+import { NgClass, AsyncPipe } from '@angular/common';
+import { SidepanelComponent } from '../sidepanel/sidepanel.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+    selector: 'app-map-layout',
+    templateUrl: './map-layout.component.html',
+    styleUrls: ['./map-layout.component.scss'],
+    standalone: true,
+    imports: [
+        NavbarComponent,
+        SidepanelComponent,
+        NgClass,
+        MapComponent,
+        AsyncPipe,
+    ],
+})
+export class MapLayoutComponent {
+  constructor(
+    protected sharedState: SharedStateService,
+    private mapService:MapService
+  ) {}
+
+  userConfig: any;
+
+  ngOnInit(): void {
+    this.sharedState.isCollapse$.subscribe((res) => {
+    this.mapService.mapResize();
+    });
+    this.sharedState.updateLoader(true);
+  }
+
+  ngAfterViewInit(): void {}
+  collapseSideBar() {
+    this.sharedState.updatedIsCollapse(false);
+  }
+}
