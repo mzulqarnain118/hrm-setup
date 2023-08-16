57f2d5ff926630104b6aec46af6ab4f54af04a6f
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-08-16 09:52:58 +0500

    navbar.component.spec.ts Updated.

--- src/app/cropscan/navbar/navbar.component.spec.ts
+++ src/app/cropscan/navbar/navbar.component.spec.ts
@@ -41,3 +41,21 @@ describe('NavbarComponent', () => {
   });
 });
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavbarComponent } from './navbar.component';
+
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [NavbarComponent]
+});
+    fixture = TestBed.createComponent(NavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+ould create', () => {
+    expect(component).toBeTruthy();
+  });
+});
