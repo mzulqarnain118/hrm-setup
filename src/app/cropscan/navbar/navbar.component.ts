d3268d88ca0f51754691757e92374e1ae6a64a68
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-08-25 16:30:30 +0500

    navbar.component.ts Updated.

--- src/app/cropscan/navbar/navbar.component.ts
+++ src/app/cropscan/navbar/navbar.component.ts
@@ -89,3 +89,47 @@ export class NavbarComponent {
 
 }
 import { Component, ElementRef, ViewChild, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { MapService } from 'src/app/_Services/map.service';
+import { SearchService } from 'src/app/_Services/search.service';
+import { SearchOption } from 'src/app/_Types/types';
+import { ExportDataComponent } from '../export-data/export-data.component';
+import { SearchComponent } from '../search/search.component';
+import { DropdownComponent } from '../dropdown/dropdown.component';
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
+    standalone: true,
+    imports: [
+        DropdownComponent,
+        SearchComponent,
+        ExportDataComponent,
+    ],
+})
+export class NavbarComponent {
+  
+  @ViewChild('dialog') dialog: ElementRef;
+
+  constructor(private mapService: MapService){}
+  reload() {
+    window.location.reload();
+  }
+
+  ImageToggle(event:any) {
+    this.mapService.toggleImagery();
+  }
+
+  openDialog() {
+    this.dialog.nativeElement.showModal();
+  }
+
+  closeDialog() {
+    this.dialog.nativeElement.close();
+  }
+  
+takeUserToAoi();
+  }
+
+}
