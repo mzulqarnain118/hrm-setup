1b975d04edc0ac85b3ef926aae56613f3f826f08
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-09-05 17:59:32 +0500

    toggle-switch.component.ts Updated.

--- src/app/cropscan/toggle-switch/toggle-switch.component.ts
+++ src/app/cropscan/toggle-switch/toggle-switch.component.ts
@@ -128,3 +128,20 @@ MenuImage = false;
   }
 }
 import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms'; // Import necessary form modules
+import { distinctUntilChanged } from 'rxjs';
+import Tileset from 'src/app/_Interfaces/Tileset';
+import { MapService } from 'src/app/_Services/map.service';
+import { colorMap } from 'src/assets/constants/colors';
+
+@Component({
+    selector: 'app-toggle-switch',
+    templateUrl: './toggle-switch.component.html',
+    styleUrls: ['./toggle-switch.component.scss'],
+    standalone: true,
+    imports: [FormsModule, ReactiveFormsModule],
+})
+export class ToggleSwitchComponent implements OnInit {
+  @Input() tileset: Tileset;
+  @Output() clickedEvent = new EventEmitter<Tileset>();
+: string;
