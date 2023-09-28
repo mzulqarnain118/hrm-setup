a92c1deb6bf8f61dbef33f5c10bc1436f65e629d
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-09-28 05:16:10 +0500

    dropdown-filter.component.spec.ts Updated.

--- src/app/dashboard/dropdown-filter/dropdown-filter.component.spec.ts
+++ src/app/dashboard/dropdown-filter/dropdown-filter.component.spec.ts
@@ -20,3 +20,18 @@ describe('DropdownFilterComponent', () => {
   });
 });
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DropdownFilterComponent } from './dropdown-filter.component';
+
+describe('DropdownFilterComponent', () => {
+  let component: DropdownFilterComponent;
+  let fixture: ComponentFixture<DropdownFilterComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [DropdownFilterComponent]
+});
+    fixture = TestBed.createComponent(DropdownFilterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+ould create', () => {
