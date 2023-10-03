ca34ce92fc5cf720fb8b008951bfebfd2dc0147a
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-10-03 07:35:11 +0500

    , dropdown-filter.component.ts, e-survey-chart.component.html, e-survey-chart.component.scss, e-survey-chart.component.spec.ts, e-survey-chart.component.ts - Files added.

---
+++ src/app/dashboard/dropdown-filter/dropdown-filter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+    selector: 'app-dropdown-filter',
+    templateUrl: './dropdown-filter.component.html',
+    styleUrls: ['./dropdown-filter.component.scss'],
+    standalone: true,
+    imports: [FormsModule]
+})
+export class DropdownFilterComponent {
+
+}
