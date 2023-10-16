40428625b1995fc96c9d473d73e82627ccd3405b
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-10-16 19:34:37 +0500

    weather-forecast-chart.component.ts Updated.

--- src/app/dashboard/weather-forecast-chart/weather-forecast-chart.component.ts
+++ src/app/dashboard/weather-forecast-chart/weather-forecast-chart.component.ts
@@ -79,3 +79,38 @@ export class WeatherForecastChartComponent {
   ];
 }
 import { Component } from '@angular/core';
+import { Color, ScaleType, LineChartModule } from '@swimlane/ngx-charts';
+
+@Component({
+    selector: 'app-weather-forecast-chart',
+    templateUrl: './weather-forecast-chart.component.html',
+    styleUrls: ['./weather-forecast-chart.component.scss'],
+    standalone: true,
+    imports: [LineChartModule],
+})
+export class WeatherForecastChartComponent {
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  showYAxisLabel: boolean = true;
+  yAxisLabel: string = 'Average Temperature';
+  colorScheme: Color = {
+    name: 'Variation',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: ['#1BC693'],
+  };
+  weatherLineChart = [
+    {
+      name: 'Weather',
+      series: [
+        {
+          name: 'Jan',
+          value: 10,
+        },
+        {
+          name: 'Feb',
+          value: 50,
+: 'Mar',
