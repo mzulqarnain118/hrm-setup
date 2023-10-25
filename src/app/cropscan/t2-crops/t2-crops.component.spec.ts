073d8836b7ddaed31ea7aeb638e345b23b25ac1a
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-10-25 09:09:15 +0500

    t2-crops.component.spec.ts added.

--- src/app/cropscan/t2-crops/t2-crops.component.spec.ts
+++ src/app/cropscan/t2-crops/t2-crops.component.spec.ts
@@ -19,3 +19,24 @@ describe('T2CropsComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { T2CropsComponent } from './t2-crops.component';
+
+describe('T2CropsComponent', () => {
+  let component: T2CropsComponent;
+  let fixture: ComponentFixture<T2CropsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [T2CropsComponent]
+});
+    fixture = TestBed.createComponent(T2CropsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
