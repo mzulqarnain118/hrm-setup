e583900b7852dc06f749de91b9e0ed26017496dc
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-10-27 19:18:23 +0500

    t2-crops.component.ts Updated.

--- src/app/cropscan/t2-crops/t2-crops.component.ts
+++ src/app/cropscan/t2-crops/t2-crops.component.ts
@@ -1,1 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  BehaviorSubject,
+  Observable,
+  combineLatest,
+  debounceTime,
+  filter,
+  map,
+  switchMap,
+} from 'rxjs';
+emoizationService } from 'src/app/_Services/Memoization/memoization.service';
