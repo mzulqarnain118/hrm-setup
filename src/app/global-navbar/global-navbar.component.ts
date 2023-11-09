371c871df19b5a5d687e55cf73ab51d44c067dd9
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-11-10 04:54:25 +0500

    global-navbar.component.ts Updated.

--- src/app/global-navbar/global-navbar.component.ts
+++ src/app/global-navbar/global-navbar.component.ts
@@ -159,3 +159,9 @@ import { Router } from '@angular/router';
 import { SharedStateService } from '../_Services/shared-state.service';
 import { combineLatest } from 'rxjs';
 t { map, filter } from 'rxjs/operators';
+import { RoleService } from '../_Services/roles.service';
+import { menuList } from 'src/assets/constants/constants';
+import { roles } from '../_Interfaces/dashboard';
+import { LogoComponent } from '../cropscan/logo/logo.component';
+import { NgClass, AsyncPipe } from '@angular/common';
+onent({
