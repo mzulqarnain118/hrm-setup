7235bb39d55cc6e885b5afab2ed80de95e470a0b
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-11-15 05:14:28 +0500

    t3-bar-chart.component.ts Updated.

--- src/app/cropscan/t3-bar-chart/t3-bar-chart.component.ts
+++ src/app/cropscan/t3-bar-chart/t3-bar-chart.component.ts
@@ -150,3 +150,52 @@ his.offset += 1;
   // paint params
   colorMap = colorMap;
 import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  OnInit,
+} from '@angular/core';
+import { Component, ViewChild, HostListener } from '@angular/core';
+import { BarHorizontalNormalizedComponent, BarChartModule } from '@swimlane/ngx-charts';
+import { debounceTime, filter, switchMap } from 'rxjs/operators';
+import { MemoizationService } from 'src/app/_Services/Memoization/memoization.service';
+import { ApiService } from 'src/app/_Services/api.service';
+import { SharedStateService } from 'src/app/_Services/shared-state.service';
+import { SidepanelService } from 'src/app/_Services/sidepanel.service';
+import { colorMap } from 'src/assets/constants/colors';
+
+@Component({
+    selector: 'app-t3-bar-chart',
+    templateUrl: './t3-bar-chart.component.html',
+    styleUrls: ['./t3-bar-chart.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+    imports: [BarChartModule],
+})
+export class T3BarChartComponent implements OnInit {
+  isDataAvailable: boolean = false;
+  @ViewChild('chart') chart: BarHorizontalNormalizedComponent;
+  multi: any[] = [];
+
+  limit: number = 15;
+  offset: number = 0;
+  reportType: string;
+  season: string;
+  reportDate: string;
+  currentBoundary: string;
+  crop: string;
+  prevScrollPosition: number = 0;
+  maxCount: number = 0;
+  unFilteredData: any[] = [];
+  constructor(
+    private sidepanelService: SidepanelService,
+    private apiService: ApiService,
+    private sharedStateService: SharedStateService,
+    private cdr: ChangeDetectorRef,
+    private memoizationService: MemoizationService
+  ) {}
+
+  ngOnInit(): void {
+    this.sidepanelService.combinedData$
+      .pipe(
+        debounceTime(100),
+ement !== '')),
