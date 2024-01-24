97a4db9e97fa2f9a7e78cf5262d3ea4001986c69
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-01-24 14:34:05 +0500

    weather-forecast-chart.component.spec.ts added.

--- src/app/dashboard/weather-forecast-chart/weather-forecast-chart.component.spec.ts
+++ src/app/dashboard/weather-forecast-chart/weather-forecast-chart.component.spec.ts
@@ -19,3 +19,24 @@ describe('WeatherForecastChartComponent', () => {
 nt).toBeTruthy();
   });
 });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WeatherForecastChartComponent } from './weather-forecast-chart.component';
+
+describe('WeatherForecastChartComponent', () => {
+  let component: WeatherForecastChartComponent;
+  let fixture: ComponentFixture<WeatherForecastChartComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [WeatherForecastChartComponent]
+});
+    fixture = TestBed.createComponent(WeatherForecastChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
