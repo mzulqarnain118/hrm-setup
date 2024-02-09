245bf9ecdd31d75cba3d0a6833593762b85d17e0
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-02-09 14:27:51 +0500

    , auth.interceptor.ts, credentials.interceptor.ts, ISidepanelData.ts - Files added.

--- src/app/Helper/auth.interceptor.ts
+++ src/app/Helper/auth.interceptor.ts
@@ -15,46 +15,11 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService :UserService, private router : Router) {}
 
   intercept(
-Request<unknown>,
-    next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
-  
-    return next.handle(request).pipe(
-      catchError((error: HttpErrorResponse) => {
-        if (error.status === 403 ) {
-          localStorage.removeItem('username'); 
-          this.authService.setAuthenticated(false); 
-          localStorage.removeItem('token'); 
-          this.router.navigate(['/signin']); 
-        }
-        return throwError(error);
-      })
-    );
-    return next.handle(request);
-  }
-}
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-  HttpErrorResponse,
-} from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
-import { UserService } from '../auth/services/user.service';
-import { Router } from '@angular/router';
-
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService :UserService, private router : Router) {}
-
-  intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
   
-e(request).pipe(
+    return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 403 ) {
           localStorage.removeItem('username'); 
