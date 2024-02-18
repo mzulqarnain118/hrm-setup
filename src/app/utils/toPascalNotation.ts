43f129281ef34d5cc2757b63ff00d8765ba9ff1c
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-02-19 04:06:14 +0500

    ndvi-chart.component.html added.

--- src/app/utils/toPascalNotation.ts
+++ src/app/utils/toPascalNotation.ts
@@ -12,3 +12,15 @@ export function toPascalCase(value: string) {
         .replace(new RegExp(/\w/), (s) => s.toUpperCase());
 }
 export function toPascalCase(value: string) {
+  return value === 'aoi'
+    ? value.toLocaleUpperCase()
+    : `${value}`
+        .toLowerCase()
+        .replace(new RegExp(/[-_]+/, 'g'), ' ')
+        .replace(new RegExp(/[^\w\s]/, 'g'), '')
+        .replace(
+          new RegExp(/\s+(.)(\w*)/, 'g'),
+          ($1, $2, $3) => `${$2.toUpperCase() + $3}`
+        )
+        .replace(new RegExp(/\w/), (s) => s.toUpperCase());
+}
