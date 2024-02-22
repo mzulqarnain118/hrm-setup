d327f47bcc5f2139aec0b5827d45195e122e165e
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-02-22 08:55:32 +0500

    role.guard.ts added.

---
+++ src/app/Services/Dashboard/role.guard.ts
@@ -0,0 +1,44 @@
+import { InjectionToken } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
+import { RoleService } from '../roles.service';
+
+export const ROLE_GUARD_TOKEN = new InjectionToken<CanActivateFn>('roleGuard');
+
+export function roleGuardFactory(roleService: RoleService, router: Router): CanActivateFn {
+  return (route: any) => {
+    const requiredPlatform = route.routeConfig.path;
+
+
+return roleService.roles$.pipe(
+  switchMap(roles => {
+    if (roles.length === 0) { 
+      return roleService.fetchRoles().pipe(
+        catchError(error => {
+          console.error('Error retrieving roles:', error);
+          router.navigate(['/signin']);
+          return of([]); 
+        })
+      );
+    } else {
+      return of(roles);
+    }
+  }),
+  switchMap(roles => {
+    console.log(roles);
+    return of(roles.some(role => role.platform_name.toLowerCase() === requiredPlatform));
+  }),
+  catchError(error => {
+    console.error('Error retrieving roles:', error);
+    router.navigate(['/signin']);
+    return of(false);
+  })
+);
+
+  };
+}
+
+export const roleGuardProvider = [
+  { provide: ROLE_GUARD_TOKEN, useFactory: roleGuardFactory, deps: [RoleService, Router] }
+];
