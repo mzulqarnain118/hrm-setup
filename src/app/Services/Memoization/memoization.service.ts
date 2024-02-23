904bbf3b0a348894ca5e40f8dee796f7e5173312
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-02-23 08:33:11 +0500

    memoization.service.ts Updated.

--- src/app/Services/Memoization/memoization.service.ts
+++ src/app/Services/Memoization/memoization.service.ts
@@ -1,1 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable, shareReplay } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemoizationService {
+  private cache = new Map<string, Observable<any>>();
+
+  memoize<T>(key: string, observable: Observable<T>): Observable<T> {
+    if (!this.cache.has(key)) {
+cache.set(key, observable.pipe(shareReplay({ bufferSize: 1, refCount: true })));
+    }
+    return this.cache.get(key) as Observable<T>;
+  }
