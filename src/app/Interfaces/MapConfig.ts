742916e94e23271e1221eb0b32ac6c428fa92571
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-02-26 04:21:19 +0500

    , MapConfig.ts, Tileset.ts, ZoomActions.ts, ZoomLevel.ts, centroid.ts, dashboard.ts, index.ts, report.ts - Files added.

--- src/app/Interfaces/MapConfig.ts
+++ src/app/Interfaces/MapConfig.ts
@@ -1,18 +1,6 @@
 export interface MapConfig {
   source: string;
   zoom: number;
-atLong?: mapboxgl.LngLat;
-  color?: string;
-}
-export interface MapConfig {
-  source: string;
-  zoom: number;
-  latLong?: mapboxgl.LngLat;
-  color?: string;
-}
-export interface MapConfig {
-  source: string;
-  zoom: number;
   latLong?: mapboxgl.LngLat;
   color?: string;
 }
