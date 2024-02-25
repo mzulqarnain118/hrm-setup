5e8a50196dfa0fedc5359ca848cad6d2c78a8e70
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-02-26 02:01:42 +0500

    layer.service.ts Updated.

--- src/app/Services/Tilesets/layer.service.ts
+++ src/app/Services/Tilesets/layer.service.ts
@@ -74,3 +74,36 @@ export class LayerService {
   }
 }
 import { Injectable } from '@angular/core';
+import Tileset from '../../_Interfaces/Tileset';
+import { getPaintAndLineWidth } from '../../_utils/paintHandler';
+import {  layerConfig } from 'src/app/_Types/types';
+@Injectable({
+  providedIn: 'root'
+})
+export class LayerService {
+
+  constructor() { }
+
+  AddFillLayer(tileset: Tileset, map: mapboxgl.Map) {
+    const layerType = 'fill';
+    const FillPrefix = layerType + tileset.name;
+
+    const { paint } = getPaintAndLineWidth(tileset.name, layerType, tileset.TilesetType);
+    let layerConfig: layerConfig = {
+      // TODO: work this ID out
+      id: FillPrefix,
+      type: 'fill',
+      source: tileset.name,
+      layout: {
+        visibility: tileset.visible ? 'visible' : 'none',
+      },
+      paint: paint,
+    };
+    if (tileset.TilesetType == 'Tileset' || tileset.TilesetType == 'Esurvey') {
+      layerConfig['source-layer'] = tileset.name;
+    }
+
+    map.addLayer(layerConfig as mapboxgl.FillLayer);
+  }
+
+map: mapboxgl.Map) {
