e1c5aeaa7d0c906e6435565a412510e5dbf69f6b
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-03-13 12:58:35 +0500

    add-commas.pipe.ts Updated.

--- src/app/Pipes/add-commas.pipe.ts
+++ src/app/Pipes/add-commas.pipe.ts
@@ -16,3 +16,18 @@ export class AddCommasPipe implements PipeTransform {
   }
 }
 import { Pipe, PipeTransform } from '@angular/core';
+import { formatNumberWithCommas } from "../_utils/formatNumberWithCommas";
+
+@Pipe({
+    name: 'addCommas',
+    standalone: true
+})
+export class AddCommasPipe implements PipeTransform {
+
+  transform(value: any): any {
+    if (!value || isNaN(Number(value))) {
+      return value;
+    }
+
+    return formatNumberWithCommas(value);
+  }
