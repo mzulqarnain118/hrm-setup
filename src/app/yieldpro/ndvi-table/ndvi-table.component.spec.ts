278c8f73405a505f476f7c18d419507c47dc5498
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-03-15 14:47:34 +0500

    ndvi-table.component.spec.ts added.

--- src/app/yieldpro/ndvi-table/ndvi-table.component.spec.ts
+++ src/app/yieldpro/ndvi-table/ndvi-table.component.spec.ts
@@ -19,3 +19,24 @@ describe('NdviTableComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NdviTableComponent } from './ndvi-table.component';
+
+describe('NdviTableComponent', () => {
+  let component: NdviTableComponent;
+  let fixture: ComponentFixture<NdviTableComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [NdviTableComponent]
+});
+    fixture = TestBed.createComponent(NdviTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
