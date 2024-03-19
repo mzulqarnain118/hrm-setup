baaf5e7a340bd24a660e3bf50fe16d15daf14b56
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-03-19 17:34:21 +0500

    ndvi-table.component.ts Updated.

--- src/app/yieldpro/ndvi-table/ndvi-table.component.ts
+++ src/app/yieldpro/ndvi-table/ndvi-table.component.ts
@@ -1,1 +1,17 @@
 import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-ndvi-table',
+    templateUrl: './ndvi-table.component.html',
+    styleUrls: ['./ndvi-table.component.scss'],
+    standalone: true
+})
+export class NdviTableComponent {
+  data = [
+    { name: "Blue", col1: 0.1, col2: 0.5 },
+    { name: "Red", col1: 0.2, col2: 0.6 },
+    { name: "Green", col1: 0.3, col2: 0.7 },
+Yellow", col1: 0.4, col2: 0.8 }
+  ];
+  
+}
